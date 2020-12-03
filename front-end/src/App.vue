bc8ff48a238c2f586645fc1fc9f1f5361f0324d2
Dennis <dbuijsman@sogyo.nl>
2020-12-03 15:56:51 +0100

    basis front-end toegevoegd

---
+++ front-end/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <a href="/" class="navbar-brand">PBS</a>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a href="/products" class="nav-link">Products</a>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {}
+</script>
---
+++ front-end/src/components/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Welcome to PBS</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class TutorialsList extends Vue {
+
+}
+</script>
