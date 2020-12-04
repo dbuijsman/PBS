ea6e8b0f10e11bdbb877f8d50583ba727d779874
Dennis <dbuijsman@sogyo.nl>
2020-12-04 12:24:08 +0100

    init Database controleert de connectie

--- stockService/products/db.py
+++ stockService/products/db.py
@@ -1,6 +1,12 @@
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 
-class Database(object):
+class Database:
     def __init__(self):
-        self.client = MongoClient("mongodb+srv://stockService:AxwY0SbZ8hVNzIUI@pbs.gv3zv.mongodb.net/PBS?retryWrites=true&w=majority")
+        self.client = MongoClient(
+            "mongodb+srv://stockService:AxwY0SbZ8hVNzIUI@pbs.gv3zv.mongodb.net/PBS?retryWrites=true&w=majority")
+        try:
+            self.client.admin.command('ismaster')
+        except ConnectionFailure:
+            print("Server not available")
